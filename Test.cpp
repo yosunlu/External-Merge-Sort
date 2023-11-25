b9e00d039b4ec7bbfc2ede7a0e2505fc3ebfec41
yushanlu <yushanlu1031@gmail.com>
2023-11-24 23:53:46 -0600

    參數設定ok

--- Test.cpp
+++ Test.cpp
@@ -3,18 +3,84 @@
 #include "Filter.h"
 #include "Sort.h"
 #include "gen.h"
+#include <iostream>
+#include <cstdlib>
+#include <unistd.h>
+#include <sys/stat.h>
 
-int main (int argc, char * argv [])
+long numOfRecord = 0;
+long record_size = 0;
+
+int main(int argc, char *argv[])
 {
-	TRACE (true);
+	TRACE(true);
+	int opt;
+	std::string output_file; // the trace file
 
-	genDataRecords(200);
-	Plan * const plan = new SortPlan (new ScanPlan (100));
+	// Using getopt to parse command line arguments
+	while ((opt = getopt(argc, argv, "c:s:o:")) != -1)
+	{
+		switch (opt)
+		{
+		case 'c':
+			// number following -c will be the number of total records
+			numOfRecord = std::atol(optarg);
+			break;
+		case 's':
+			record_size = std::atoi(optarg);
+			break;
+		case 'o':
+			output_file = optarg;
+			break;
+		default: /* '?' */
+			std::cerr << "Usage: " << argv[0] << " -c records -s size -o output_file" << std::endl;
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	// Check if all required arguments are provided
+	if (numOfRecord == 0 || record_size == 0 || output_file.empty())
+	{
+		std::cerr << "All options -c, -s, and -o must be provided" << std::endl;
+		exit(EXIT_FAILURE);
+	}
+
+	const char *directoryName = "input";
+	struct stat info;
+
+	// Check if the directory exists
+	if (stat(directoryName, &info) != 0)
+	{
+		// Directory does not exist, try to create it
+		int dirCreationResult = mkdir(directoryName, 0777);
+		if (dirCreationResult == 0)
+		{
+			std::cout << "Directory created successfully." << std::endl;
+		}
+		else
+		{
+			std::cerr << "Error creating directory." << std::endl;
+		}
+	}
+	else if (info.st_mode & S_IFDIR)
+	{
+		// Directory exists
+		std::cout << "Directory already exists." << std::endl;
+	}
+	else
+	{
+		// Path exists but is not a directory
+		std::cerr << "Path exists but is not a directory." << std::endl;
+	}
+
+	// create records that will be saved in the input file
+	genDataRecords(numOfRecord);
+	Plan *const plan = new SortPlan(new ScanPlan(numOfRecord));
 	// Plan * const plan = new FilterPlan ( new ScanPlan (7) );
 	// new SortPlan ( new FilterPlan ( new ScanPlan (7) ) );
 
-	Iterator * const it = plan->init ();
-	it->run ();
+	Iterator *const it = plan->init();
+	it->run();
 	delete it;
 
 	delete plan;
